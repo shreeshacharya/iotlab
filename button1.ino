8416cba3040199e3a082184fd5f28d8dbeaa0dcd
shreeshacharya <143174861+shreeshacharya@users.noreply.github.com>
2025-08-11 10:44:50 +0530

    Add files via upload

---
+++ button1.ino
@@ -0,0 +1,46 @@
+// C++ code
+//
+/*
+  Button
+  Turns on and off a light emitting diode(LED)
+  connected to digital  pin 13, when pressing a
+  pushbutton attached to pin 2.
+
+   The circuit:
+  * LED attached from pin 13 to ground
+  * pushbutton attached to pin 2 from +5V
+  * 10K resistor attached to pin 2 from ground
+  * Note: on most Arduinos there is already an LED
+  on the board  attached to pin 13.
+
+  created 2005  by DojoDave <http://www.0j0.org>
+
+  modified 30 Aug 2011  by Tom Igoe
+
+  This example code is in the public domain.
+  http://www.arduino.cc/en/Tutorial/Button
+*/
+
+int buttonState = 0;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // read the state of the pushbutton value
+  buttonState = digitalRead(2);
+  // check if pushbutton is pressed.  if it is, the
+  // buttonState is HIGH
+  if (buttonState == HIGH) {
+    // turn LED on
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    // turn LED off
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
