734093d359bf1c166d2cc9708d7d567d7841b13c
shreeshacharya <143174861+shreeshacharya@users.noreply.github.com>
2025-08-23 09:41:32 +0530

    Add files via upload

---
+++ photoresistor1.ino
@@ -0,0 +1,25 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(9,INPUT);
+  pinMode(10,OUTPUT);
+  Serial.begin(9600);
+    
+}
+
+void loop()
+{
+ int temp=digitalRead(9);
+  Serial.println("WILL THE LIGHT GLOW=");
+  Serial.println(temp);
+  delay(300);
+  if(temp==HIGH)
+    digitalWrite(10,HIGH);
+  else
+     digitalWrite(10,LOW);
+
+    
+  
+  
+}
